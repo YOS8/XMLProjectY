69d2823ff2acf92965820451a137d96626df3981
macbook <macbook@MacBook-Pro-de-macbook-2.local>
2017-12-29 16:52:52 +0100

    Tab to XML script

--- tabToXML.py
+++ tabToXML.py
@@ -6,7 +6,7 @@
 # In[10]:
 
 with open("/Users/nino/Desktop/cour_M2/xml/projet/xml/Dico_Français_Klingon2.tab", "r") as f:
-    with open("/Users/nino/Desktop/cour_M2/xml/projet/xml/Dico_Français_Klingon2.xml","w") as fic:
+    with open("/Users/nino/Desktop/cour_M2/xml/projet/xml/Dico_Français_Klingon3.xml","w") as fic:
         fic.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n")
         fic.write("<!DOCTYPE Root SYSTEM \"Dico_Français_klingon.dtd\">\n")
         fic.write("<racine>\n")
@@ -19,6 +19,8 @@ with open("/Users/nino/Desktop/cour_M2/xml/projet/xml/Dico_Français_Klingon2.t
             fic.write("<mot id=\"{}\">\n".format(i))
             fic.write("\t<mot_fr>{}</mot_fr>\n".format(splits[0]))
             fic.write("\t<mot_klg>{}</mot_klg>\n".format(splits[1]))
+            fic.write("\t<phrase_fr>NULL</phrase_fr>\n")
+            fic.write("\t<phrase_klg>NULL</phrase_klg>\n")
             fic.write("</mot>\n")
             fic.write("\n")
         fic.write("</racine>")
